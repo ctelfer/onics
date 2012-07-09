a239a5bbd8e8e2f4525904a21cbfb7275dd0db84
Chris Telfer <ctelfer@gmail.com>
2012-07-09 00:18:51 -0400

    Add lexical analysis tests.

---
+++ tests/pml_lex_test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+TBIN=bin
+TOUT=tmp
+
+rm -fr $TOUT/*
+ERR=0
+
+
+lex_test()
+{
+	echo -----------------
+	echo -n "Lexical Analysis Test $1: "
+
+	head -1 data/pml/parse_test_$1.pml | sed -e 's/# *//'
+
+	${TBIN}/testpmllex < data/pml/parse_test_$1.pml \
+		> tmp/lex_test_$1.out 2> $TOUT/lex_test_$1.err
+
+	if cmp data/pml/lex_test_$1.out $TOUT/lex_test_$1.out &&
+	   cmp data/pml/lex_test_$1.err $TOUT/lex_test_$1.err
+	then
+		echo PASSED
+	else
+		echo FAILED
+		ERR=1
+	fi
+
+	echo -----------------
+	echo
+}
+
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13
+do
+	lex_test $i
+done
+
+exit $ERR
--- tests/src/testpmllex.c
+++ tests/src/testpmllex.c
@@ -19,11 +19,47 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-int testpmllex();
+#include <stdio.h>
+
+
+typedef void *pml_scanner_t;
+typedef void *pml_buffer_t;
+int pmllex_init(pml_scanner_t *);
+void pmlset_in(FILE *input, pml_scanner_t);
+pml_buffer_t pml_scan_string(const char *, pml_scanner_t);
+void pml_delete_buffer(pml_buffer_t, pml_scanner_t);
+int pmllex(pml_scanner_t);
+struct pml_lex_val pmlget_extra(pml_scanner_t);
+void pmlset_extra(struct pml_lex_val v, pml_scanner_t);
+const char *pmlget_text(pml_scanner_t);
+int pmlget_lineno(pml_scanner_t);
+void pmllex_destroy(pml_scanner_t);
+
+extern const char *pml_tok_strs[];
+
 
 int pmlwrap(void)
 {
 	return 1;
+}
+
+
+int testpmllex()                                                                    
+{       
+        int x;
+        pml_scanner_t scanner;
+        if ( pmllex_init(&scanner) )
+                errsys("pmllex_init:");                                             
+        pmlset_in(stdin, scanner);
+        while ( (x = pmllex(scanner)) > 0 )
+                printf("%-15s'%s'\n", pml_tok_strs[x], pmlget_text(scanner));       
+        if ( x < 0 ) {
+                printf("unknown char on line: %d\n", pmlget_lineno(scanner));       
+        } else {
+                printf("End of file\n");                                            
+        }
+        pmllex_destroy(scanner);                                                    
+        return 0;                                                                   
 }
 
 int main(int argc, char *argv[])
