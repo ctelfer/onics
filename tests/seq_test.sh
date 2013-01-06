4640575b668bff153b2535829b7d64fca68872be
Chris Telfer <ctelfer@gmail.com>
2013-01-06 01:14:03 -0500

    Add more tests to cover sequencing and fix bugs.

--- tests/seq_test.sh
+++ tests/seq_test.sh
@@ -62,6 +62,19 @@ seqtest() {
 
 
 seqtest 1 "Basic test" NONE ""
-seqtest 2 "IP Fragment ID test" "-c -ipid" "-c -ipid"
+seqtest 2 "IP Frag ID test" "-c -ipid" "-c -ipid"
+seqtest 3 "IP Frag ID + Eth dst test" "-c -ipid -edst" "-c -ipid -edst"
+seqtest 4 "Eth dst test" "-c -edst" "-c -edst"
+seqtest 5 "Eth src test" "-c -esrc" "-c -esrc"
+seqtest 6 "IP dst test" "-c -ipdst" "-c -ipdst"
+seqtest 7 "IP src test" "-c -ipsrc" "-c -ipsrc"
+seqtest 8 "IPv6 Flowid" "-c -v6fid" "-c -v6fid"
+seqtest 9 "TCP payload bytes 0-3" "-c -tpld 0,4" "-c -tpld 0,4"
+seqtest 10 "UDP payload bytes 0-7" "-c -upld" "-c -upld"
+seqtest 11 "ICMP payload bytes 4-5" "-c -ipld 4,2" "-c -ipld 4,2"
+seqtest 12 "ICMPv6 payload bytes 4-5" "-c -i6pld 4,2" "-c -i6pld 4,2"
+seqtest 13 "Big mix" \
+	"-c -tpld -ipid -v6fid -edst" \
+	"-c -tpld -ipid -v6fid -edst"
 
 exit $RET
